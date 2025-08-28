89e3afb51bf24489e512686c65f8fb1ad7c98fa1
Franco Zelener <franco.zelener-1@corteva.com>
2025-08-27 21:00:15 -0300

    Initial Commit

---
+++ ios-challenge-uala/ios_challenge_ualaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_challenge_ualaApp.swift
+//  ios-challenge-uala
+//
+//  Created by Franco Zelener on 27/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ios_challenge_ualaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-challenge-ualaTests/ios_challenge_ualaTests.swift
@@ -0,0 +1,16 @@
+//
+//  ios_challenge_ualaTests.swift
+//  ios-challenge-ualaTests
+//
+//  Created by Franco Zelener on 27/08/2025.
+//
+
+import Testing
+
+struct ios_challenge_ualaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
